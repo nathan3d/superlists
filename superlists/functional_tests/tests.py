96ccf09f0f7564e94f2204313f67c83a7db25432
Nathan Brizendine <nathanb@allocone.com>
2015-03-18 16:04:27 +0700

    Add a url for creating new list via POST

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -63,7 +63,7 @@ class NewVisitorTest(LiveServerTestCase):
         self.browser.quit()
         self.browser = webdriver.Firefox()
 
-        # Francis visits the home page. THere is no sign of Edith's
+        # Francis visits the home page. There is no sign of Edith's
         # list
         self.browser.get(self.live_server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
