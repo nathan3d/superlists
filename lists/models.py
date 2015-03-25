8147fd3fb6be66b8691d32fed44decdda6ac7c57
Nathan Brizendine <nathanb@allocone.com>
2015-03-25 15:27:38 +0700

    use form in all views, back to working state

--- lists/models.py
+++ lists/models.py
@@ -9,5 +9,5 @@ class List(models.Model):
 
 
 class Item(models.Model):
-    text = models.TextField(default='')
+    text = models.TextField(default='', blank=False)
     list = models.ForeignKey(List, default=None)
